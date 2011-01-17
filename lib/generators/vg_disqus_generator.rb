065c34a7067dbf15716c2566b48596465326c145
Ahmad Varoqua <ahmad.varoqua@gmail.com>
2011-01-16 16:27:44 -0800

    VGDISQUS > Dev > Creating install generator take 2

--- lib/generators/vg_disqus_generator.rb
+++ lib/generators/vg_disqus_generator.rb
@@ -3,19 +3,6 @@ require 'rails/generators'
 module VgDisqus
   module Generators
 
-    class InstallGenerator < Rails::Generators::Base
-
-      include Thor::Actions
-      include Thor::Shell
-
-      source_root File.expand_path('../templates', __FILE__)
-
-      # Copy over shared partial to render the view
-      def copy_partial
-        copy_file "app/views/shared/_disqus.html.haml", "app/views/shared/_disqus.html.haml"
-      end
-    end
-
     class VgDisqusGenerator < Rails::Generators::Base
 
       include Thor::Actions
